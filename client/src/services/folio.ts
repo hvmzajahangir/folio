58f22f1911690d293ebf046a6282faf8613e4849
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-03-04 14:46:12 +0000

    feat: delete watchlist item

--- client/src/services/folio.ts
+++ client/src/services/folio.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { WatchlistItem } from "../types";
+import { WatchlistItem, WatchlistResponse } from "../types";
 
 const API_BASE_URL: string = process.env.NEXT_PUBLIC_FOLIO_API_BASE_URL || "";
 
@@ -8,14 +8,24 @@ export const folioApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: API_BASE_URL,
   }),
+  tagTypes: ["Watchlist"],
   endpoints: (builder) => ({
     getWatchlist: builder.query<[WatchlistItem], string>({
       query: (userId) => `/watchlist/${userId}`,
+      providesTags: ["Watchlist"],
       transformResponse: (response: any) => {
         return response.result;
       },
     }),
+    deleteWatchlistItem: builder.mutation<WatchlistResponse, number>({
+      query: (id) => ({
+        url: `/watchlist/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Watchlist"],
+    }),
   }),
 });
 
-export const { useGetWatchlistQuery } = folioApi;
+export const { useGetWatchlistQuery, useDeleteWatchlistItemMutation } =
+  folioApi;
