ddd6b8884fff966e8a69026c149257cf33d85c6e
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-03-04 15:04:10 +0000

    style: highlight current path in sidebar

--- client/src/components/DashboardLayout/Sidebar/index.tsx
+++ client/src/components/DashboardLayout/Sidebar/index.tsx
@@ -3,9 +3,14 @@ import { useRouter } from "next/router";
 
 const Sidebar = (): ReactElement => {
   const router = useRouter();
+  const path = router.pathname;
   const pushToRoute = (route: string) => {
     router.push(route);
   };
+  const activeRouteClass =
+    "inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100";
+  const routeClass =
+    "inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200";
   return (
     <aside className="z-20 hidden w-64 overflow-y-auto bg-gradient-to-r from-gray-800 via-gray-900 to-black md:block flex-shrink-0">
       <div className="py-4 text-gray-500 dark:text-gray-400">
@@ -22,11 +27,15 @@ const Sidebar = (): ReactElement => {
               pushToRoute("/dashboard");
             }}
           >
-            <span
-              className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-              aria-hidden="true"
-            ></span>
-            <a className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100">
+            {path === "/dashboard" && (
+              <span
+                className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"
+              ></span>
+            )}
+            <a
+              className={path === "/dashboard" ? activeRouteClass : routeClass}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -58,7 +67,15 @@ const Sidebar = (): ReactElement => {
               pushToRoute("/watchlist");
             }}
           >
-            <a className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+            {path === "/watchlist" && (
+              <span
+                className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"
+              ></span>
+            )}
+            <a
+              className={path === "/watchlist" ? activeRouteClass : routeClass}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
